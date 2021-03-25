ace56e4e2518193779bd34cc96feab00f207df6c
Dzung Pean <hoangtudauphong@gmail.com>
2021-03-24 20:49:58 -0700

    happy num

---
+++ string/happy number.py
@@ -0,0 +1,12 @@
+# till sum of nums =1
+class Solution:
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum(int(i) ** 2 for i in str(n))
+        return n == 1
