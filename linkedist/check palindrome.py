4e6998f20953dcc9b8f1deb0ea238e08d2157e21
Dzung Pean <hoangtudauphong@gmail.com>
2021-04-12 10:07:45 -0700

    check palindrome

---
+++ linkedist/check palindrome.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# palindrome go backward the same as forward
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        vals = []
+        while head:
+            vals.append(head.val)
+            head = head.next
+        return vals == vals[::-1]
