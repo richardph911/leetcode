0f6f438e8b2672d8f12c8c505b1ce3dea7616457
Dzung Pean <hoangtudauphong@gmail.com>
2021-04-19 10:53:21 -0700

    same tree

---
+++ tree/same tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+    
+# class Solution:
+    # def isSameTree(self, p, q):
+    #     if p and not q or q and not p: return False
+    #     if not p and not q: return True
+    #     return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
