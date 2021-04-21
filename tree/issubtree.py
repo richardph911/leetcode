d681c8fd133c380884942c2abc53815483e65eb2
Dzung Pean <hoangtudauphong@gmail.com>
2021-04-20 21:53:37 -0700

    subtree in tree

--- tree/issubtree.py
+++ tree/issubtree.py
@@ -6,19 +6,35 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        # case s is null, t is defined so no need to check if null
-        if s is None:
-            return False
-        if self.sameTree(s,t):
+        # 1st method using string to compare print preorder
+        sTree = helper(s)
+        tTree = helper(t)
+        if tTree in sTree:
             return True
-        return self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
+        return False
+# must be outside of class to call. otherwise need to be inside isSubtree function
+def helper(root):
+        if root is None:
+            return "Null"
+            
+        return "$" + str(root.val) + ' ' + helper(root.left) + ' ' + helper(root.right)
+            
+        
+        # 2nd method using recursive
+#     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+#         # case s is null, t is defined so no need to check if null
+#         if s is None:
+#             return False
+#         if self.sameTree(s,t):
+#             return True
+#         return self.isSubtree(s.left,t) or self.isSubtree(s.right,t)
     
-    def sameTree(self, s, t)->bool:
-        if t is None and s is None:
-            return True
-        if t is None or s is None:
-            return False
-        return s.val == t.val and self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+#     def sameTree(self, s, t)->bool:
+#         if t is None and s is None:
+#             return True
+#         if t is None or s is None:
+#             return False
+#         return s.val == t.val and self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
     
         
         
