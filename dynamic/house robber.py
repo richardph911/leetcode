2f37f8178ed6b42b9b4ea9ebac7fdf15e0247bd7
Dzung Pean <hoangtudauphong@gmail.com>
2021-05-01 12:13:37 -0700

    house robber explainat.

--- dynamic/house robber.py
+++ dynamic/house robber.py
@@ -1,3 +1,14 @@
+#For example: num = [1,7,9,4], at the beginning, max_3_house_before, max_2_house_before, adjacent are initialized to 0, so it is like putting 3 zeros before the input list [0, 0, 0, 1, 7, 9, 4]. Here are steps for calculating the max sum for each house(the sliding window is marked by parentheses):
+#
+#[(0, 0, 0, 1), 7, 9, 4], cur = max(0+1, 0+1)
+#
+#-> [ (0, 0, 1, 7), 9, 4], cur = max(0+7, 0+7)
+#
+#-> [(0, 1, 7, 9), 4], cur = max(0+9, 1+9)
+#
+#-> [(1, 7, 10, 4)], cur = max(1+4, 7+4)
+#
+#-> [7, 10, 11], 10 is the max sum of path that includes num[-2], 11 is the max sum of path that includes num[-1], so return max(10, 11)
 class Solution:
     
     def rob(self, nums):
