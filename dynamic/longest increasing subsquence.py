b8a90f9e355e09fc1cbfb92d3d90af8e8b43e4e6
Dzung Pean <hoangtudauphong@gmail.com>
2021-05-02 23:43:07 -0700

    Update longest increasing subsquence.py

--- dynamic/longest increasing subsquence.py
+++ dynamic/longest increasing subsquence.py
@@ -1,14 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        n = len(nums)
-        dp = [1] * n
-
-        for i in range(1, n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], 1 + dp[j])
-                    
-        return max(dp)
+        res = []
+        for num in nums:
+            for i, v in enumerate(res):
+                if num <= v:
+                    res[i] = num
+                    break
+            else:
+                res.append(num)
+        return len(res)
