fc9a184eba9b911334ca818e1cf2a82b0929f911
Dzung Pean <hoangtudauphong@gmail.com>
2021-05-03 22:06:56 -0700

    valid parentheses

---
+++ string/valid parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == "(" or char == "{" or char == "[":
+                stack.append(char)
+            elif len(stack) <= 0:
+                return False
+            elif char == ")" and stack.pop() != "(":
+                return False
+            elif char == "]" and stack.pop() != "[":
+                return False
+            elif char == "}" and stack.pop() != "{":
+                return False
+        if len(stack) == 0:
+            return True
+        return False
