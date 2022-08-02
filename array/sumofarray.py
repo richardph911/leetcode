7ac8e4c8d5cb04e33de451fcfb9910e80c916977
Dzung Pean <hoangtudauphong@gmail.com>
2022-08-02 13:04:12 -0700

    sum of 1d

---
+++ array/sumofarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        output = []
+        #sum += index
+        sum = 0
+        for i in nums:
+            sum += i
+            output.append(sum)
+        return output
