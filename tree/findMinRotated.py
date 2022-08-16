8036d483215b4db379091869a90b1144b537835e
Dzung Pean <hoangtudauphong@gmail.com>
2022-08-16 13:26:33 -0700

    Update findMinRotated.py

--- tree/findMinRotated.py
+++ tree/findMinRotated.py
@@ -4,9 +4,11 @@ class Solution:
         left, right = 0, len(nums)-1
         # from 0 to len(nums) - 1
         while left <= right:
+            # value left < right so that would be the min
             if nums[left] < nums[right]:
                 res = min(res, nums[left])
                 break
+            #otherwise, binary search with middle
             mid = (left + right) //2
             res = min(res, nums[mid])
             #search if in left portion
