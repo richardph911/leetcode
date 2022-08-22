6892aa9097161e154414ac58769beb23ebcf95a0
Dzung Pean <hoangtudauphong@gmail.com>
2022-08-22 07:30:38 -0700

    linked list cycle

--- linkedist/cycle linked list.py
+++ linkedist/cycle linked list.py
@@ -15,3 +15,22 @@ class Solution:
             if(fast == slow):
                 return True
         return False
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # since only tail has cycle.
+        headSet = set()
+        while head is not None:
+            if head in headSet:
+                return True
+            headSet.add(head)
+            head = head.next
+        return False
+    # time 0(n) : visit each of the n elements at most once. Adding a node to the hash takes 0(1) time
+    # space 0(n): the space depends on the number of elements added to the has table
