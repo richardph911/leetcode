ad9d6523c611d783e4b10c9416b6892504fc8af8
Dzung Pean <hoangtudauphong@gmail.com>
2022-10-04 14:00:13 -0700

    longest consecutive sequence

--- array/longestConsecutive.py
+++ array/longestConsecutive.py
@@ -12,3 +12,19 @@ class Solution:
                 res = max(res, right - i + 1)
         return res
         
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of a sequence
+            # only check if that number is the start. if not, ignore.
+            # if that start, then add up the start which is 1 + length --> 2->3>4
+            #return the longest length
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
